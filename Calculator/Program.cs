b138df4de114869ce0698361ccb4d29a5e1e9b42
Opilane TTHK <opilane@tthk.ee>
2021-10-19 11:33:52 +0300

    functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Sisesta esimene number");
+            int firstnumber = Convert.ToInt32(Console.ReadLine) 
+
+
+        
+        
+         }
+
+
+        private static void AddTwoNumbers (int x ,int y)
+
+        
+        
+        
+        
+        
+        {
+            int result = x + y;
+            Console.WriteLine($"{x}+{y} = {result}");
+            
+
+        }
+         private static void DivideTwoNumber(double x ,double y)  
+        
+    
+    
+    }
+}
